148c2e1d7a4d490e950432eb315690922736e972
WickyNilliams <WickyNilliams@users.noreply.github.com>
2024-05-30 12:01:22 +0100

    add help command

--- bin/index.js
+++ bin/index.js
@@ -22,11 +22,30 @@ const {
       type: "string",
       short: "i",
     },
+    help: {
+      type: "boolean",
+      short: "h",
+    },
   },
 });
 
+const text = `Usage: file-gauge "**/*.js"
+
+Print raw/min/gzip sizes of files matching the glob
+
+  -i, --ignore   A glob pattern of files to ignore
+  -l, --limit    A KB file limit. Files appear red if exceeding this, or yellow if within 10% of this
+  -m, --minify   Minify the files before min/brotli. Only works with JavaScript files
+  -h, --help     Print help
+`;
+
+if (values.help) {
+  console.log(text);
+  process.exit();
+}
+
 if (!glob) {
-  console.error('Glob pattern required. Usage:\n\nfile-gauge "*.js"');
+  console.error(`Glob pattern required. See --help`);
   process.exit(1);
 }
 
